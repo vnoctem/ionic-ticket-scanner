097f9ee4749f56f669fbe252d74caeb62edc4085
Chun <chun-hei.chu.1@etsmtl.net>
2017-01-29 16:11:16 -0500

    Add loading page prototype

--- mobile/src/app/app.component.ts
+++ mobile/src/app/app.component.ts
@@ -1,30 +1,18 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { AuthentificationPage } from '../pages/authentification/authentification';
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
-
-  rootPage: any = Page1;
-
-  pages: Array<{title: string, component: any}>;
+  rootPage: any = AuthentificationPage;
 
   constructor(public platform: Platform) {
     this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
-    ];
-
   }
 
   initializeApp() {
@@ -34,11 +22,5 @@ export class MyApp {
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
-  }
-
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
   }
 }
